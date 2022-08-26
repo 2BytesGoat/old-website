11c7f212eb44b7377988dac9f5eeec7d4ae5206e
AnaIordache <aiordache22@gmail.com>
2022-08-26 15:47:32 +0300

    fixed cover width

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import cover from '../media/cover.gif';
+import "./Home.css";
 
 const home = () => (
   <div>
-     <img src={cover} alt="cover" />
+     <img src={cover} alt="cover" className="cover-img"/>
   </div>
 );
 
